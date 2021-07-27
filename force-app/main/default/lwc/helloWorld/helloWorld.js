748e6a3a0b401cb80b65f44353f2870f3a46925c
Matheus Antonio <matheusantonio.info@gmail.com>
2021-07-26 21:26:06 -0300

    Local Properties and Data Binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+   
+    fullName = 'Zero to Hero'
+    users    = ['a', 'b'] // Not allowed in template
+    user      = {
+        name: 'Matheus'
+    }
+   
+}
