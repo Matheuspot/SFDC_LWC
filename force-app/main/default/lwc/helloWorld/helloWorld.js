f8b5b118ff1f13bce47a84d5f1ebe7de864dffde
Matheus Antonio <matheusantonio.info@gmail.com>
2021-07-26 22:18:02 -0300

    Methods and Two way Data Binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,11 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {
-   
+export default class HelloWorld extends LightningElement {   
     fullName = 'Zero to Hero'
-    users    = ['a', 'b'] // Not allowed in template
-    user      = {
-        name: 'Matheus'
-    }
-   
+    title = 'Aura'
+
+    changeHandler(event) {
+        this.title = event.target.value
+    }   
 }
