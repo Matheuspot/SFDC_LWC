c7d51e8bf2cfd6def0b56c16b0e5c8e67b75d662
Matheus Antonio <matheusantonio.info@gmail.com>
2021-07-28 20:46:27 -0300

    @track properties

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,10 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {   
     fullName = 'Zero to Hero'
-    title = 'Aura'
+    title    = 'Aura'
 
     changeHandler(event) {
         this.title = event.target.value
     }   
+
+    trackHandler(event) {
+        this.address = {...this.address, 'city' : event.target.value}
+    }
+
+    address = {
+        city     : 'Melbourne',
+        postCode : 3008,
+        country  : 'Australia'
+    }
 }
