c881b92bb570c9911b1ab689f3cfa69f7a204726
Matheus Antonio <matheusantonio.info@gmail.com>
2021-07-28 21:05:16 -0300

    Getters in LWC

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -4,17 +4,36 @@ export default class HelloWorld extends LightningElement {
     fullName = 'Zero to Hero'
     title    = 'Aura'
 
+    @track address = {
+        city     : 'Melbourne',
+        postCode : 3008,
+        country  : 'Australia'
+    }
+
+    @track userLst = ['a', 'b', 'c']
+
     changeHandler(event) {
         this.title = event.target.value
     }   
 
     trackHandler(event) {
-        this.address = {...this.address, 'city' : event.target.value}
+        this.address.city = event.target.value        
+        //this.address = {...this.address, 'city' : event.target.value}
     }
 
-    address = {
-        city     : 'Melbourne',
-        postCode : 3008,
-        country  : 'Australia'
+    // getter example
+    users       = ['Matheus', 'Marcos', 'Rafael']    
+    num1        = 10
+    num2        = 20
+ 
+    get firstUser() {
+        return this.users[0].toUpperCase()
+    } 
+
+    get multiply() {
+        return this.num1 * this.num2
     }
+
+
+    
 }
