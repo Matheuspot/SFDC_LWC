0ffbd3a7e9b589324134de001d48d42f4319afb6
Matheus Antonio <matheusantonio.info@gmail.com>
2021-10-09 20:42:26 -0300

    Wire Apex Method with Params - My Version

--- force-app/main/default/lwc/wireApexWithParams/wireApexWithParams.js
+++ force-app/main/default/lwc/wireApexWithParams/wireApexWithParams.js
@@ -1,18 +1,19 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track} from 'lwc';
 import filterAccountTypeType from '@salesforce/apex/AccountController.filterAccountTypeType';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import TYPE_FIELD from '@salesforce/schema/Account.Type';
 
 export default class WireApexWithParams extends LightningElement {
-    selectedType = ''
+    
+    selectedType = ''  
+
     @wire(filterAccountTypeType, {type: '$selectedType'}) filteredAccounts  
+    @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT }) accountInfo;
+    @wire(getPicklistValues, { recordTypeId: '$accountInfo.data.defaultRecordTypeId', fieldApiName: TYPE_FIELD}) accountTypeValues;  
 
-    get typeOptions() {
-        return [
-            {label: "Customer - Channel", value: "Customer - Channel"},
-            {label: "Customer - Direct",  value: "Customer - Direct"},
-        ]
-    }
-
-    typeHandler(event) {
+    handleChange(event) {
         this.selectedType = event.target.value
     }
 }
