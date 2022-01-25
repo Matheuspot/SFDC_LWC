02e87e22b34fad0d0cb95730c11db621c9094644
Matheus Antonio <matheus.silva7@raizen.com>
2022-01-24 21:55:33 -0300

    lightning-record-view-form

---
+++ force-app/main/default/lwc/recordViewFormDemo/recordViewFormDemo.js
@@ -0,0 +1,18 @@
+import { LightningElement, api } from 'lwc';
+
+export default class RecordViewFormDemo extends LightningElement {
+   
+    @api objectApiName
+    /*
+    @api recordId
+    
+    
+    get accountRecordId() {
+        if (this.recordId == undefined) {
+            this.recordId = '0015e000005ccdLAAQ'
+        }
+        return this.recordId
+    }
+    */
+}
+    
