1e410bbd108e6560f956f69982420795ebb28f5d
Matheus Antonio <matheus.silva7@raizen.com>
2022-01-29 20:29:32 -0300

    Pagination v1.3 (Not working)

--- force-app/main/default/lwc/listRecordsToSelect/listRecordsToSelect.js
+++ force-app/main/default/lwc/listRecordsToSelect/listRecordsToSelect.js
@@ -2,17 +2,18 @@ import { LightningElement, wire, track} from 'lwc';
 import getAccountList from '@salesforce/apex/AccountController.getAccountList';
 import getSelectedRows from '@salesforce/apex/AccountController.setSelectedRows';
 
+const columns = [
+                    { label: 'Account Name', fieldName: 'Name', type: 'text', sortable: true},
+                    { label: 'Account Type', fieldName: 'Type', type: 'text', sortable: true}
+                ];
 export default class ListRecordsToSelect extends LightningElement {
 
-@track columns = 
-    [
-        { label: 'Account Name', fieldName: 'Name', type: 'text', sortable: true},
-        { label: 'Account Type', fieldName: 'Type', type: 'text', sortable: true}
-    ];
+@track columns = columns 
 
 @track rowsToDisplay = []
-@track allRowsToDisplay = []
+@track allSelectedRows = []
 @track selectedRows = []
+@track currentSelectedRows = []
 @track lsAccounts = []
 @track error
 @track paginatedItems = []
@@ -56,82 +57,60 @@ export default class ListRecordsToSelect extends LightningElement {
     }
 
     getSelectedRows() {
-        this.selectedRows = this.template.querySelector('lightning-datatable').getSelectedRows();       
-        this.checkSelectedRow()       
+        this.selectedRows = this.template.querySelector('lightning-datatable').getSelectedRows();      
+        this.setSelectedRow()       
     }
 
-    checkSelectedRow() {    
-        let newRowsId = []   
-        let oldRowsId = []   
-        let arrRemovedIds = []
-        let arrAddedIds = []
-        let filteredArray = []
-        
-        // filter to pop removed rows Id
-        oldRowsId = this.getDeselectedRows()           
+    setSelectedRow() {    
 
-        // filter to include only new rows Id
-        newRowsId = this.selectedRows
-            .filter(item => !this.allRowsToDisplay.includes(item.Id)).map(item => item.Id)  
-
+        let aux = []
+        aux = this.selectedRows
+            .filter(element => !this.currentSelectedRows.includes(element.Id))
+            .map(element => element.Id)
         
-        arrRemovedIds = this.allRowsToDisplay
-        .filter(item => (!oldRowsId.includes(item.Id)))    
-        .map(item => item.Id)
-
-        console.log('removed: ' + arrRemovedIds)     
-    
-        arrAddedIds = this.allRowsToDisplay
-        .filter(item => (!newRowsId.includes(item.Id)))
-        .map(item => item.Id)
-        
-        console.log('added: ' + arrAddedIds)  
-    
-
-       this.allRowsToDisplay = [...this.allRowsToDisplay, newRowsId ]
-        
-                 
-        
-
-        console.log('res:' + this.allRowsToDisplay)      
-            //.concat(filteredArray.filter(item => !this.allRowsToDisplay.includes(item.Id)))
+       console.log('aux: ' + aux)
        
-    }    
-    getDeselectedRows() {
+       
+        this.currentSelectedRows.push(aux)
 
-        let deselectedRecs = []
-        let auxArray = []
+        console.log('current: ' + this.currentSelectedRows)
 
-        if (this.auxArray.length > this.selectedRows.length) {                     
-            deselectedRecs = this.auxArray
-                .filter(x => !this.selectedRows.includes(x))
-                .concat(this.selectedRows.filter(x => !this.auxArray.includes(x)));                                          
-        }
-
-        this.auxArray = this.selectedRows 
-        auxArray = deselectedRecs.map(item => item.Id)
+            for(let row of this.lsAccounts){
+                if(!this.allSelectedRows.includes(row) && this.selectedRows.includes(row)){
+                    this.allSelectedRows.push(row)
+                }
+              
+                
+                if(this.allSelectedRows.includes(row) && !this.selectedRows.includes(row)){
+                    let index = this.allSelectedRows.indexOf(row)
+                    this.allSelectedRows.splice(index, 1)
+                }
+                
+            }
         
-        if (auxArray.length < 1) return ''
-        if (auxArray.length > 0) return auxArray        
+        
+        
     }
 
     next() {  
         let hasNextPage = this.page < this.totalPage - 1
         if (hasNextPage) this.page++   
-      
-        this.selectedRowToDisplay = this.allRowsToDisplay           
+        
+        //this.setSelectedRows() 
+        this.selectedRowToDisplay = this.allSelectedRows 
+          
         this.update()    
-
-        console.log('next: ' + this.allRowsToDisplay)        
+        //console.log('next: ' + JSON.stringify(this.allSelectedRows))        
     }
 
     prev() {
         let hasPrevPage = this.page > 0
         if (hasPrevPage) this.page--
- 
-        this.selectedRowToDisplay = this.allRowsToDisplay       
+
+        //this.setSelectedRows()   
+        this.selectedRowToDisplay = this.allSelectedRows       
         this.update()           
-        console.log('prev: ' + this.allRowsToDisplay)      
+       // console.log('prev: ' + JSON.stringify(this.allSelectedRows))      
     }
 
     update() {        
