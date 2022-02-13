7e26079578ca6a27e3b2dc27abc32349a8db1398
Matheus Antonio <matheus.silva7@raizen.com>
2022-02-13 17:11:06 -0300

    Aura Component: Client-side search filter v1.2 - Working Properly

--- force-app/main/default/aura/LightningSearchDataTable/LightningSearchDataTableController.js
+++ force-app/main/default/aura/LightningSearchDataTable/LightningSearchDataTableController.js
@@ -1,10 +1,12 @@
 ({
     init : function (cmp, event, helper) {
+        
         cmp.set('v.columns', [
             { label: 'Id', fieldName: 'id', type: 'text' },
             { label: 'Name', fieldName: 'name', type: 'text' },
             { label: 'Industry', fieldName: 'industry', type: 'text' }
         ]);
+
         var action = cmp.get("c.fetchAccountData");
         action.setCallback(this, function(response){
             var state = response.getState();
@@ -15,50 +17,49 @@
         });
         $A.enqueueAction(action);
     },
+
     searchTable : function(cmp,event,helper) {  
         
         let fetchedData = cmp.get("v.data");
         let filteredValues = []
        
         let fieldName  = event.getSource().get("v.name")
-        let fieldValue = event.getSource().get("v.value")     
-        let savedAccountName  = ''
-        if (fieldName == 'accountName') {
-            savedAccountName = fieldValue
-        }      
+        let fieldValue = event.getSource().get("v.value")
+  
+        if (fieldName == 'accountName') cmp.set("v.accountName", fieldValue)        
         if (fieldName == 'accountIndustry') cmp.set('v.accountIndustry', fieldValue)
 
-       
-        let savedAccountIndustry = cmp.get('v.accountIndustry') == undefined ? '' :  cmp.get('v.accountIndustry')
-    
-        var tempArray = []
-        for(let i = 0; i < fetchedData.length; i++){
-            if(
-                (fetchedData[i].name && fetchedData[i].name.toUpperCase().indexOf(savedAccountName.toUpperCase()) != -1) && 
-                (fetchedData[i].industry && fetchedData[i].industry.toUpperCase().indexOf(savedAccountIndustry.toUpperCase()) != -1) 
-            ){
-               
-                tempArray.push(fetchedData[i]);
-                
+        let savedAccountName = cmp.get('v.accountName')
+        let savedAccountIndustry = cmp.get('v.accountIndustry')  
+
+        console.log(savedAccountName)
+        console.log(savedAccountIndustry)
+        
+        let searchedFields = {}
+     
+        searchedFields = JSON.parse(`   
+                                    {
+                                        "name" : "${savedAccountName}", 
+                                        "industry" : "${savedAccountIndustry}"
+                                    }`
+                                )
+
+        Object.keys(searchedFields).forEach(key => {
+            if (searchedFields[key] === "undefined" || searchedFields[key] === "") {
+                delete searchedFields[key];
             }
-        }
+        });
 
-        if (savedAccountName == '' &&  savedAccountIndustry == '') {
-            tempArray = []
-        }
-
-        console.log('saved acc: '  + savedAccountName)
-        console.log('saved industry: ' + savedAccountIndustry)
-        console.log('ARRAY: ' + JSON.stringify(tempArray))
-
-        if (tempArray.length > 0 ) {
-            cmp.set('v.filteredData', tempArray)
-        } else {
-            cmp.set('v.filteredData', fetchedData) 
-        }
-
-
-
-           
+        fetchedData.filter(item => {           
+            for (let key in searchedFields) {             
+                if (!item[key].toUpperCase().includes(searchedFields[key].toUpperCase())) {                    
+                    return false
+                }
+            }              
+            filteredValues.push(item)  
+        });
+      
+        if (filteredValues.length > 0) cmp.set('v.filteredData', filteredValues)
+        if (filteredValues.length < 1) cmp.set('v.filteredData', fetchedData)          
     }  
 })
