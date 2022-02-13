456126df9b9857a41b1c4e5773cc692ff38aeacb
Matheus Antonio <matheus.silva7@raizen.com>
2022-02-13 15:10:23 -0300

    Aura Component: Client-side search filter v1.0

--- force-app/main/default/aura/LightningSearchDataTable/LightningSearchDataTableController.js
+++ force-app/main/default/aura/LightningSearchDataTable/LightningSearchDataTableController.js
@@ -1,11 +1,11 @@
 ({
-    init: function (cmp, event, helper) {
+    init : function (cmp, event, helper) {
         cmp.set('v.columns', [
-            { label: 'First Name', fieldName: 'FirstName', type: 'text' },
-            { label: 'Last Name', fieldName: 'LastName', type: 'text' },
-            { label: 'Email', fieldName: 'Email', type: 'text' }
+            { label: 'Id', fieldName: 'id', type: 'text' },
+            { label: 'Name', fieldName: 'name', type: 'text' },
+            { label: 'Industry', fieldName: 'industry', type: 'text' }
         ]);
-        var action = cmp.get("c.fetchData");
+        var action = cmp.get("c.fetchAccountData");
         action.setCallback(this, function(response){
             var state = response.getState();
             if (state === "SUCCESS") {
@@ -15,21 +15,49 @@
         });
         $A.enqueueAction(action);
     },
-    searchTable : function(cmp,event,helper) {
-        var allRecords = cmp.get("v.data");
-        var searchFilter = event.getSource().get("v.value").toUpperCase();
+    searchTable : function(cmp,event,helper) {  
         
-        var tempArray = [];
-        var i;
+        let fetchedData = cmp.get("v.data");
+        let filteredValues = []
+       
+        let fieldName  = event.getSource().get("v.name")
+        let fieldValue = event.getSource().get("v.value")
+  
+        if (fieldName == 'accountName') cmp.set("v.accountName", fieldValue)        
+        if (fieldName == 'accountIndustry') cmp.set('v.accountIndustry', fieldValue)
 
-        for(i=0; i < allRecords.length; i++){
-            if((allRecords[i].FirstName && allRecords[i].FirstName.toUpperCase().indexOf(searchFilter) != -1) ||
-               (allRecords[i].LastName && allRecords[i].LastName.toUpperCase().indexOf(searchFilter) != -1 ) || 
-               (allRecords[i].Email && allRecords[i].Email.toUpperCase().indexOf(searchFilter) != -1 ) )
-            {
-                tempArray.push(allRecords[i]);
-            }
-        }
-        cmp.set("v.filteredData",tempArray);
-    }
+        let savedAccountName = cmp.get('v.accountName')
+        let savedAccountIndustry = cmp.get('v.accountIndustry')  
+
+        console.log(savedAccountName)
+        console.log(savedAccountIndustry)
+        
+        let searchedFields = {}
+     
+        searchedFields = JSON.parse(
+            `{
+                "name" : "${savedAccountName}", 
+                "industry" : "${savedAccountIndustry}"
+            }`
+        )
+                   
+
+        console.log('searched fields: ' + JSON.stringify(searchedFields))    
+        console.log('fetchedData: ' + JSON.stringify(fetchedData))
+
+        fetchedData.filter(item => {           
+            for (let key in searchedFields) {             
+                if (item[key] === undefined || item[key].toUpperCase() != searchedFields[key].toUpperCase()) {
+                    console.log('passed here 1')
+                    return false
+                }
+            }   
+            console.log('passed here 2')
+            filteredValues.push(item)  
+        });
+
+        console.log('filtered value array: ' + filteredValues)
+        if (filteredValues.length > 0) cmp.set('v.filteredData', filteredValues)
+        if (filteredValues.length < 1) cmp.set('v.filteredData', fetchedData)          
+    }  
 })
