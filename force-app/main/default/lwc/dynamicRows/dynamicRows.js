e587110740b5658becb439f7b196639db60c1178
Matheus Antonio <matheusantonio.info@gmail.com>
2022-05-07 14:09:23 -0300

    Dynamic Fields v1.1

--- force-app/main/default/lwc/dynamicRows/dynamicRows.js
+++ force-app/main/default/lwc/dynamicRows/dynamicRows.js
@@ -1,12 +1,13 @@
 import { LightningElement, track } from 'lwc';
 import getAccountIndustryByName from '@salesforce/apex/DynamicFieldsController.getAccountIndustryByName'
-const OBJECT_ROW = {'Id' : '', 'Name' : '', 'Industry' : ''}
+import getAccountPhoneByName from '@salesforce/apex/DynamicFieldsController.getAccountPhoneByName'
+const OBJECT_ROW = {'Id' : '', 'Name' : '', 'Industry' : '', 'Phones' : []}
 
 export default class DynamicRows extends LightningElement {
 
 @track rows = []
 @track rowModel = {}
-
+@track options = []//[{label:'39722047', value :'39722047'}, {label:'39722048', value :'39722047'}]
 type
 numberOfRows = 0
 objectRow = OBJECT_ROW
@@ -17,16 +18,18 @@ objectRow = OBJECT_ROW
     
     searchForAccountIndustry(event) {
        
-        let name = event.target.value
         let index = event.target.dataset.index
+        let fieldName = event.target.name
+        let accountName = event.target.value 
      
-        getAccountIndustryByName({accountName : name})
-        .then(result =>{
-            this.buildObjectByController(result, index)                
+        getAccountIndustryByName({accountName : accountName})
+        .then(data =>{
+            this.buildObjectByController('Industry', data, index)                
         }).catch(error => {
-            this.error = error
-            console.log(this.error)
+            this.error = error         
         })
+
+        this.setPhoneOptions('Phones', accountName, index)
     }
 
     buildRow() {        
@@ -64,18 +67,46 @@ objectRow = OBJECT_ROW
         }       
     }
 
-    buildObjectByController(result, index) {
-        for (let i = 0; i < this.rows.length; i++){
-            if (this.rows[i].Id === parseInt(index)) {
-                this.rows[i]['Industry'] = result
-            }
-        }    
+    buildObjectByController(fieldName, data, index) {
+
+        console.log('here')
+        console.log(fieldName)
+
+        if (fieldName === 'Industry') {
+            for (let i = 0; i < this.rows.length; i++){
+                if (this.rows[i].Id === parseInt(index)) {
+                    this.rows[i]['Industry'] = data
+                }
+            }  
+        }
+
         console.log(JSON.stringify(this.rows))
+
+        if (fieldName === 'Phones') {
+            for (let i = 0; i < this.rows.length; i++){
+                if (this.rows[i].Id === parseInt(index)) {
+                    this.rows[i]['Phones'] = data
+                }
+            }  
+        }        
+    }
+
+    setPhoneOptions(fieldName, accountName, index) {
+        let options = []
+
+        getAccountPhoneByName({accountName : accountName})
+        .then(data =>{  
+            for (let i = 0; i < data.length; i++) {
+                options.push({value : data[i].Phone, label : data[i].Phone})
+            } 
+            this.buildObjectByController(fieldName, options, index)            
+        }).catch(error => {
+            console.error(error)
+        })
     }
 
     setNumberOfRows(event) {
-        this.numberOfRows = parseInt(event.target.value)    
-        console.log(this.numberOfRows)    
+        this.numberOfRows = parseInt(event.target.value)         
     }
 
     deleteRow(event) {
