251604c790113da1e506ecd9ac608b7cb0304ab8
Matheus <matheusantonio.info@gmail.com>
2022-07-03 13:51:27 -0300

    My Custom Lookup Project

--- force-app/main/default/lwc/lookUpWithMultiSelection/lookUpWithMultiSelection.js
+++ force-app/main/default/lwc/lookUpWithMultiSelection/lookUpWithMultiSelection.js
@@ -1,3 +1,61 @@
 import { LightningElement } from 'lwc';
 
-export default class LookUpWithMultiSelection extends LightningElement {}
+const CLOSED_COMBOBOX = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click'
+const OPEN_COMBOBOX = CLOSED_COMBOBOX + ' slds-is-open'
+const CLOSED_INPUT = 'slds-input slds-combobox__input'
+const OPEN_INPUT = CLOSED_INPUT + ' slds-has-focus'
+export default class LookUpWithMultiSelection extends LightningElement {
+
+  comboboxState = CLOSED_COMBOBOX
+  inputState = CLOSED_INPUT
+
+  isInputOut = true
+  isMouseOut = true
+
+  set comboActualState (value) {
+    this.comboboxState = value
+  }
+
+  get comboActualState() {
+    return this.comboboxState
+  }
+
+  onFocusIn() { 
+    this.isInputOut = false
+    this.openCombobox() 
+  }
+
+  onFocusOut() {
+    this.isInputOut = true
+    this.openCombobox()   
+  }
+
+  onMouseOver() {
+    this.isMouseOut = false
+    this.openCombobox() 
+  }
+
+  onMouseLeave() {
+    this.isMouseOut = true
+    this.openCombobox()
+  }
+
+  openCombobox() {
+    if ( this.isInputOut && this.isMouseOut ) {
+      this.displayCombobox(false)   
+    }
+    else {
+      this.displayCombobox(true)   
+    }
+  }
+
+  displayCombobox(value) {
+    if (value) {
+      this.comboActualState = OPEN_COMBOBOX
+      this.inputState = OPEN_INPUT      
+    } else {
+      this.comboActualState = CLOSED_COMBOBOX
+      this.inputState = CLOSED_INPUT
+    }
+  }
+}
